e41a9a7548c348688f944cdb867de0a263098239
Weliff Lima <weliff.14@gmail.com>
2018-03-04 00:31:00 -0300

    adicionado fetatures module, core module e shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
-import { TabViewModule } from 'primeng/primeng';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    TabViewModule
+    BrowserAnimationsModule,
+
+    LancamentosModule,
+    PessoasModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lancamentos/lancamentos.module.ts
@@ -0,0 +1,48 @@
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    SelectButtonModule,
+    CalendarModule,
+    InputTextareaModule,
+    DropdownModule,
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule,
+
+    CurrencyMaskModule,
+
+    SharedModule
+  ],
+  declarations: [
+    LancamentoCadastroComponent,
+    LancamentosPesquisaComponent,
+    LancamentosGridComponent
+  ],
+  exports: [
+    LancamentoCadastroComponent,
+    LancamentosPesquisaComponent
+  ]
+  })
+export class LancamentosModule { }
