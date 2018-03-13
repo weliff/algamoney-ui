1ee795289f0a90221cc63a34cecb7ff39d88bfd3
Weliff Lima <weliff.14@gmail.com>
2018-03-13 01:01:46 -0300

    atualizado versão do primeng, adicionado server com node/express

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -11,8 +11,10 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
+  exibindoMenu = false;
+
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private logoutService: LogoutService,
     private errorHandler: ErrorHandlerService,
     private router: Router
